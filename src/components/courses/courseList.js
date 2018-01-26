0d1045c9f8cfe4758353321b79e335521dc7cdab
Riley Harrington <riley@paribus.co>
2018-01-26 17:12:26 -0500

    link to author manage page?

--- src/components/courses/courseList.js
+++ src/components/courses/courseList.js
@@ -22,7 +22,7 @@ var CourseList = React.createClass({
                     <td><a href="#" onClick={this.deleteCourse.bind(this, course.id)}>Delete</a></td>
                     <td><Link to="editCourse" params={{id: course.id}}>{course.id}</Link></td>
                     <td><a href={course.watchHref}>{course.title}</a></td>
-                    <td>{course.author}(todo: add name)</td>
+                    <td><Link to="editAuthor" params={{id: course.author}}>{course.author}</Link></td>
                     <td>{course.length}</td>
                     <td>{course.category}</td>
                 </tr>
